c837dcc554a426de474ed268d6ce02e6ebc953db
Jorge Fernandez <jorgelfc1989@gmail.com>
2022-03-28 14:34:39 -0400

    completed DAO Tests

--- src/test/java/com/revature/daotests/CustomerDAOTest.java
+++ src/test/java/com/revature/daotests/CustomerDAOTest.java
@@ -25,7 +25,7 @@ public class CustomerDAOTest {
 		   Customer lp = new Customer(25, "Lewis Parker", 20000.00, "Checking Account", "Savings Account");
 			dao.createCustomer(lp);
 			CustomerDAOTest.customer = lp;
-			Assertions.assertEquals(25, lp.getCustomerId(), 20000.00, "CheckingAccount");
+			Assertions.assertEquals(25, lp.getCustomerId());
 	  }
 @Test
 @Order(2)
@@ -35,6 +35,46 @@ void getAllCustomersTest() {
 	allCustomers = dao.getCustomers();
 	Assertions.assertNotEquals(noCustomers, allCustomers);
 }
-	  
+
+@Test
+@Order(3)
+void getCustomerByIdTest() {
+	Customer cus = new Customer(10, "Jorge",30000.00, "Checking Account", "Savings Account");
+	dao.getCustomerById(10);
+	CustomerDAOTest.customer = cus;
+	Assertions.assertEquals(10, cus.getCustomerId());
+}
+@Test
+@Order(4)
+void updateCustomerByIdTest() {
+	Customer cus = new Customer(15, "Troy",25000.00,"Savings Account", "Checking Account");
+	dao.updateCustomerById(cus, 15);
+	CustomerDAOTest.customer= cus;
+	Assertions.assertEquals(15, cus.getCustomerId());
+}
+@Test
+@Order(5)
+void deleteCustomerTest() {
+	Customer cus = new Customer(25,"Lewis Parker", 20000.00, "Checking Account", "Savings Account");
+	dao.deleteCustomer(25);
+	CustomerDAOTest.customer = cus;
+	Assertions.assertEquals(25, cus.getCustomerId());
+}
+@Test
+@Order(6)
+void createNewAccountTest() {
+	Customer cus = new Customer(25,"Lewis Parker", 20000.00, "Checking Account", "Savings Account");
+	dao.createAccountById(cus, 25);
+	CustomerDAOTest.customer = cus;
+	Assertions.assertEquals(25, cus.getCustomerId());
+}
+@Test
+@Order(7)
+void getAccountByIdTest() {
+	Customer cus = new Customer(25,"Lewis Parker", 20000.00, "Checking Account", "Savings Account");
+	dao.getAccountForCustomerById(25, "Lewis");
+	CustomerDAOTest.customer = cus;
+	Assertions.assertEquals("Checking Account", cus.getAccountName());
+}
 	 
 }
